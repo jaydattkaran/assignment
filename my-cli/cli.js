c71d5e4ab0faca2de40d0f2899d7a24b99de769d
Jaydatt Karan <karanjaydatt03@gmail.com>
2024-09-23 02:18:05 +0530

    inital commit

---
+++ my-cli/cli.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+// CLI tool to describe yourself
+console.log(`
+    Hello! I'm Jaydatt.
+    - Deep divn into Web3 technologies.
+    - Forging in crafting intuitive, minimalistic designs.
+    - I also enjoy going to the gym to stay fit.
+    `);
+    
